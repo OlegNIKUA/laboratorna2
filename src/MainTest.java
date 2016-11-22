f6454f4dcbd01084446244d4b1679dcdc318f29c
Oleg <oleg-yarmolenko@outlook.com>
2016-11-22 19:42:39 +0200

    Initial commit

---
+++ src/MainTest.java
@@ -0,0 +1,22 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by OlegY on 18.10.2016.
+ */
+public class MainTest {
+    @org.junit.Before
+    public void setUp() throws Exception {
+
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void sui() throws Exception {
+
+    }
+
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+
+    public int sui(int x, int y){
+       int k = x+y;
+        return k;
+    }
+    public static void main(String[] args) {
+
+
+
+	// write your code here
+    }
+}
